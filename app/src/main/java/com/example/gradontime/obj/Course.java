27a630dd9569525ab259837a84ce4add75d2134b
Tanner Mills <tanner.mills2000@gmail.com>
2023-03-03 14:07:05 -0700

    Created instance variables for Course.java and added getters/setters for the class.

--- app/src/main/java/com/example/gradontime/obj/Course.java
+++ app/src/main/java/com/example/gradontime/obj/Course.java
@@ -1,6 +1,7 @@
 package com.example.gradontime.obj;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 enum Status {
     PLANNED,
@@ -11,7 +12,56 @@ enum Status {
 
 public class Course extends Todo {
     private Status status;
+    private ArrayList<Assessment> assessmentList = new ArrayList<>();
+
+    /**
+     * Creates a new course. Status is set to Status.PLANNED by default.
+     * @param title the title of the course.
+     * @param startDate the start date of the course.
+     * @param endDate the end date of the course.
+     */
     Course(String title, LocalDate startDate, LocalDate endDate) {
         super(title, startDate, endDate);
+        status = Status.PLANNED;
+    }
+
+    /**
+     * Adds a newly created assessment to the assessment list.
+     * @param assessment the assessment to add.
+     */
+    public void addAssessment(Assessment assessment) {
+        assessmentList.add(assessment);
+    }
+
+    /**
+     * Removes an assessment from the assessment list.
+     * @param assessment the assessment to remove.
+     */
+    public void removeAssessment(Assessment assessment) {
+        assessmentList.remove(assessment);
+    }
+
+    /**
+     * Gets the status of the course.
+     * @return  either PLANNED, CURRENT, COMPLETE or DROPPED
+     */
+    public Status getStatus() {
+        return status;
+    }
+
+    /**
+     * Sets the status of the course.
+     * @param status either PLANNED, CURRENT, COMPLETED or DROPPED
+     */
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    /**
+     * Checks to see if the list of assessments associated with this course is empty.
+     * @return true if the list of assessments is empty, false otherwise.
+     */
+    public boolean hasAppointments() {
+        return assessmentList.isEmpty();
     }
 }
