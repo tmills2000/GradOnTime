6458bcdf444072d34db237282403a4219c69f08a
Tanner Mills <tanner.mills2000@gmail.com>
2023-03-03 12:45:21 -0700

    Refactored some files and added a placeholder text for the terms.

--- app/src/main/java/com/example/gradontime/ui/TermList.java
+++ app/src/main/java/com/example/gradontime/ui/TermList.java
@@ -1,14 +1,17 @@
-package com.example.gradontime;
+package com.example.gradontime.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.gradontime.R;
+
+public class TermList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_term_list);
     }
+
 }
