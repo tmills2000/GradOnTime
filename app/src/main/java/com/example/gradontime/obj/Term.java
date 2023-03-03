9211c6517df12e5c5ee502eb460e165f8ead4893
Tanner Mills <tanner.mills2000@gmail.com>
2023-03-03 14:14:22 -0700

    Added an ArrayList of courses to the Term class. Added the associated getters/setters for them.

--- app/src/main/java/com/example/gradontime/obj/Term.java
+++ app/src/main/java/com/example/gradontime/obj/Term.java
@@ -1,10 +1,28 @@
 package com.example.gradontime.obj;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Term extends Todo{
+    private ArrayList<Course> courseList = new ArrayList<>();
     private
     Term(String title, LocalDate startDate, LocalDate endDate) {
         super(title, startDate, endDate);
+    }
+
+    /**
+     * Adds a course to the list of courses associated with the Term object.
+     * @param course the course to add.
+     */
+    public void addCourse(Course course) {
+        courseList.add(course);
+    }
+
+    /**
+     * Removes a course from the list of courses associated with the Term object.
+     * @param course the course to remove.
+     */
+    public void removeCourse(Course course) {
+        courseList.remove(course);
     }
 }
