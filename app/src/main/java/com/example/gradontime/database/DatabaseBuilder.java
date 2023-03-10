eb0d4fd12cf3b0c886c1a4b6217ccf0679e5918f
Tanner Mills <tanner.mills2000@gmail.com>
2023-03-10 12:30:33 -0700

    Created the DatabaseBuilder.java class

---
+++ app/src/main/java/com/example/gradontime/database/DatabaseBuilder.java
@@ -0,0 +1,34 @@
+package com.example.gradontime.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.gradontime.DAO.TermDAO;
+import com.example.gradontime.obj.Assessment;
+import com.example.gradontime.obj.Course;
+import com.example.gradontime.obj.Term;
+
+@Database(entities = {Term.class, Course.class, Assessment.class}, version = 1, exportSchema = false)
+public abstract class DatabaseBuilder extends RoomDatabase {
+    public abstract TermDAO termDAO();
+    public abstract CourseDAO courseDAO;
+    public abstract AssessmentDAO assessmentDAO;
+
+    private static volatile DatabaseBuilder INSTANCE;
+
+    static DatabaseBuilder getDatabase(final Context context) {
+        if(INSTANCE == null) {
+            synchronized (DatabaseBuilder.class) {
+                INSTANCE = Room.databaseBuilder(context.getApplicationContext(), DatabaseBuilder.class, "gradDatabase")
+                        .fallbackToDestructiveMigration()
+                        .build();
+            }
+
+        }
+        return INSTANCE;
+    }
+
+}
